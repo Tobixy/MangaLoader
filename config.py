bf3b63068f9b5fdd1591e1a081868ca9246439a0
Tobixy <shivamrajput69bs@gmail.com>
2024-11-04 04:05:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ['28374181'])
-    API_HASH = os.environ['00b7ca7f535e816590db39e76f85d0c7']
-    BOT_TOKEN = os.environ['6696415252:AAGcw_Z_0IIx8eOBf88V1pG4lKkd36DoZdY']
-    OWNER_ID = int(os.environ['5715764478'])
-    AS_ZIP = bool(os.environ['AS_ZIP'])
+    APP_ID = 28374181  # Replace with your actual APP_ID as an integer
+    API_HASH = "00b7ca7f535e816590db39e76f85d0c7"  # Replace with your actual API_HASH
+    BOT_TOKEN = "6696415252:AAGcw_Z_0IIx8eOBf88V1pG4lKkd36DoZdY"  # Replace with your actual BOT_TOKEN
+    OWNER_ID = 5715764478  # Replace with your actual OWNER_ID as an integer
+    AS_ZIP = True  # Set this to True or False as needed
