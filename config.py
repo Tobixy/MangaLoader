21c859d4692e72ebe5a53f95c0df52d1e14e97cf
Tobixy <shivamrajput69bs@gmail.com>
2024-11-04 03:59:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ['APP_ID'])
-    API_HASH = os.environ['API_HASH']
-    BOT_TOKEN = os.environ['BOT_TOKEN']
-    OWNER_ID = int(os.environ['OWNER_ID'])
+    APP_ID = int(os.environ['28374181'])
+    API_HASH = os.environ['00b7ca7f535e816590db39e76f85d0c7']
+    BOT_TOKEN = os.environ['6696415252:AAGcw_Z_0IIx8eOBf88V1pG4lKkd36DoZdY']
+    OWNER_ID = int(os.environ['5715764478'])
     AS_ZIP = bool(os.environ['AS_ZIP'])
